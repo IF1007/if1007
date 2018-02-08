a7d300f9bb4ee945edcd41f5cec51fe27d208e73
Vinicius Cardoso Garcia <vcg@cin.ufpe.br>
2018-02-08 16:39:49 -0300

    Adding first part of lecture 6 source code

---
+++ sourcecode/lecture6/lecture6a.search/src/main/java/com/brownfield/pss/search/controller/SearchRestController.java
@@ -0,0 +1,50 @@
+package com.brownfield.pss.search.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brownfield.pss.search.component.SearchComponent;
+import com.brownfield.pss.search.entity.Flight;
+
+@RefreshScope
+@CrossOrigin
+@RestController
+@RequestMapping("/search")
+class SearchRestController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SearchComponent.class);
+	
+	private SearchComponent searchComponent;
+	
+	@Value("${originairports.shutdown:SEA}")
+	private String originAirportShutdownList;
+	
+	@Autowired
+	public SearchRestController(SearchComponent searchComponent){
+		this.searchComponent = searchComponent;
+	}
+	
+	@RequestMapping(value="/get", method = RequestMethod.POST)
+	List<Flight> search(@RequestBody SearchQuery query){
+		logger.info("Input : "+ query);
+		if(Arrays.asList(originAirportShutdownList.split(",")).contains(query.getOrigin())){
+		    logger.info("The origin airport is in shutdown state");
+		    return new ArrayList<Flight>();
+		}
+		//System.out.println("Input : "+ query);
+		return searchComponent.search(query);
+	}
+ 
+}
